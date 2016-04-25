edc930d118209a82c97ff28a14fc21c4d1448f67
Dylan Dunne <dylanadunne@gmail.com>
2016-04-25 13:35:22 -0400

    final version with page display

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
-for (var n = 1; n <= 100; n++) {
-  var output = "";
-  if (n % 3 == 0)
-    output += "Fizz";
-  if (n % 5 == 0)
-    output += "Buzz";
-  console.log(output || n);
+document.getElementById('fizzbuzzOutput');
+for(var n = 1; n <=100; n++){
+
+    if(n % 15 === 0){
+        fizzbuzzOutput.innerHTML = fizzbuzzOutput.innerHTML   + "FizzBuzz";
+        fizzbuzzOutput.innerHTML = fizzbuzzOutput.innerHTML   + "<br/>";
+    
+    }else if(n % 3 === 0){
+        fizzbuzzOutput.innerHTML = fizzbuzzOutput.innerHTML   + "Fizz";
+        fizzbuzzOutput.innerHTML = fizzbuzzOutput.innerHTML   + "<br/>";
+    
+    }else if(n % 5 === 0){
+        fizzbuzzOutput.innerHTML = fizzbuzzOutput.innerHTML   + "Buzz";
+       fizzbuzzOutput.innerHTML = fizzbuzzOutput.innerHTML   + "<br/>";
+    
+    }else{
+       fizzbuzzOutput.innerHTML = fizzbuzzOutput.innerHTML   + n;
+       fizzbuzzOutput.innerHTML = fizzbuzzOutput.innerHTML   + "<br/>";
+    }
+
 }
