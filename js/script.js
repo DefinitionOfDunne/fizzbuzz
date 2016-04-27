233f40b360424a2cd021a59dea1eedc2cfaefa25
Dylan Dunne <dylanadunne@gmail.com>
2016-04-27 12:01:19 -0400

    refactored with user input

--- js/script.js
+++ js/script.js
@@ -1,21 +1,34 @@
-document.getElementById('fizzbuzzOutput');
-for(var n = 1; n <=100; n++){
-
-    if(n % 15 === 0){
-        fizzbuzzOutput.innerHTML = fizzbuzzOutput.innerHTML   + "FizzBuzz";
-        fizzbuzzOutput.innerHTML = fizzbuzzOutput.innerHTML   + "<br/>";
+$(document).ready(function() {
     
-    }else if(n % 3 === 0){
-        fizzbuzzOutput.innerHTML = fizzbuzzOutput.innerHTML   + "Fizz";
-        fizzbuzzOutput.innerHTML = fizzbuzzOutput.innerHTML   + "<br/>";
+    var inputNumber = parseInt(prompt("Enter a number between 1 and 100."));
+    if (isNaN(inputNumber)) {
+        prompt("Please use numerical digits.");
+    }
+    else if (inputNumber % 1 !== 0) {
+        prompt("Enter a whole number.");
+    }
+    else {
+        fizzbuzz(inputNumber);
+    }
+function fizzbuzz() {
+    for (var n = 1; n <= inputNumber; n++) {
     
-    }else if(n % 5 === 0){
-        fizzbuzzOutput.innerHTML = fizzbuzzOutput.innerHTML   + "Buzz";
-       fizzbuzzOutput.innerHTML = fizzbuzzOutput.innerHTML   + "<br/>";
-    
-    }else{
-       fizzbuzzOutput.innerHTML = fizzbuzzOutput.innerHTML   + n;
-       fizzbuzzOutput.innerHTML = fizzbuzzOutput.innerHTML   + "<br/>";
+        if (n % 5 === 0 && n % 3 === 0) {
+            $("ul").append('FizzBuzz');
+        }
+        else if (n % 5 === 0) {
+            $("ul").append('Buzz');
+        }
+        else if (n % 3 === 0) {
+            $("ul").append('Fizz');
+        }
+        else {
+            $("ul").append(n);
+        }
+        $("ul").append("<br>");
     }
 
-}
+};
+
+});
+
