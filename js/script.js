8abb35326c0a1ad89fc0db604070d72f61eb8af7
Dylan Dunne <dylanadunne@gmail.com>
2016-04-28 00:44:39 -0400

    added css file and updated design

--- js/script.js
+++ js/script.js
@@ -1,34 +1,62 @@
-$(document).ready(function() {
+/*jshint
+  expr: true
+*/
+
+/*
+ When I click on a button of class fb-generate
+ I want to take the context of an input field of class fb-input
+ and pass it into a fizzbuzz function 
+ */
+
+$(document).ready(function () {
+
+    // var inputNumber = Math.floor(prompt("Enter a number between 1 and 100."));
+
+    $('.fb-button').click(function () {
+        var inputNumber = $('.fb-input').val();
+
+        if (isNaN(inputNumber)) {
+            $('.fb-message').text("Please enter an integer.");
+        } else {
+            $('.fb-input').empty();
+            fizzbuzz(inputNumber);
+        }
     
-    var inputNumber = parseInt(prompt("Enter a number between 1 and 100."));
-    if (isNaN(inputNumber)) {
-        prompt("Please use numerical digits.");
-    }
-    else if (inputNumber % 1 !== 0) {
-        prompt("Enter a whole number.");
-    }
-    else {
-        fizzbuzz(inputNumber);
-    }
-function fizzbuzz() {
-    for (var n = 1; n <= inputNumber; n++) {
+    $(".call-out").append(inputNumber + "?!?");
     
-        if (n % 5 === 0 && n % 3 === 0) {
-            $("ul").append('FizzBuzz');
+    setTimeout(function() {
+        $(".call-out").empty();
+        }, 2000);
+    });
+
+
+
+    function fizzbuzz(inputNumber) {
+        $('ul li').remove();
+        
+
+        for (var n = 1; n <= inputNumber; n++) {
+            n % 5 !== 0 && n % 3 !== 0 ? append(n) : false;
+            n % 5 === 0 ? append('Buzz') : false;
+            n % 3 === 0 ? append('Thizz') : false;
+            n % 3 === 0 && n % 5 ? append('ThizzBuzz') : false;
+
+            // ternary operator
+            /*
+                if(x > 5) {
+                console.log('Its greater');
+                } else {
+                console.log('Its less');
+                }
+                
+                this same statement ternary way
+                x > 5 ? console.log('Its greater') : console.log('Its less');
+             */
+
         }
-        else if (n % 5 === 0) {
-            $("ul").append('Buzz');
-        }
-        else if (n % 3 === 0) {
-            $("ul").append('Fizz');
-        }
-        else {
-            $("ul").append(n);
-        }
-        $("ul").append("<br>");
     }
 
-};
-
+    function append(item) {
+        $(".fb-list").append('<li>' + item + '</li>');
+    }
 });
-
