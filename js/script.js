d4b180c2bea61eceeaf4e4670af41ed640150880
Dylan Dunne <dylanadunne@gmail.com>
2016-04-29 17:56:48 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,62 +1,41 @@
-/*jshint
-  expr: true
-*/
-
-/*
- When I click on a button of class fb-generate
- I want to take the context of an input field of class fb-input
- and pass it into a fizzbuzz function 
- */
 
 $(document).ready(function () {
-
-    // var inputNumber = Math.floor(prompt("Enter a number between 1 and 100."));
-
+​
     $('.fb-button').click(function () {
-        var inputNumber = $('.fb-input').val();
-
-        if (isNaN(inputNumber)) {
-            $('.fb-message').text("Please enter an integer.");
+        var inputNumber = parseInt($('.fb-input').val());
+​
+        if (isNaN(inputNumber) || inputNumber > 100) {
+​
+            $('.fb-message').text("Yo bro, enter an integer brah, between 1 & 100 yo.");
+​
         } else {
+​
             $('.fb-input').empty();
+            $('.fb-message').empty();
+​
             fizzbuzz(inputNumber);
+​
+            $(".call-out").empty();
+            $(".call-out").append(inputNumber + "?!?");
         }
-    
-    $(".call-out").append(inputNumber + "?!?");
-    
-    setTimeout(function() {
-        $(".call-out").empty();
-        }, 2500);
     });
-
-
-
+​
     function fizzbuzz(inputNumber) {
+​
         $('ul li').remove();
-        
-
-        for (var n = 1; n <= inputNumber; n++) {
-        n % 5 !== 0 && n % 3 !== 0 ? append(n) : false;
-        n % 15 === 0 ? append('ThizzBuzz') : false;
-        n % 3 === 0 ? append('Thizz') : false;
-        n % 5 === 0 ? append('Buzz') : false;
-
-            // ternary operator
-            /*
-                if(x > 5) {
-                console.log('Its greater');
-                } else {
-                console.log('Its less');
-                }
-                
-                this same statement ternary way
-                x > 5 ? console.log('Its greater') : console.log('Its less');
-             */
-
+​
+        for (var n = 1; n < inputNumber; n++) {
+​
+            n % 5 !== 0 && n % 3 !== 0 ? append(n) : false;
+            n % 15 === 0 ? append('ThizzBuzz') : false;
+            n % 3 === 0 ? append('Thizz') : false;
+            n % 5 === 0 ? append('Buzz') : false;
+​
         }
     }
-
+​
     function append(item) {
         $(".fb-list").append('<li>' + item + '</li>');
     }
+​
 });
