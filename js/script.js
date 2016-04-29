1d447e5b6e5172d7adb954f3115f75061d7cab14
Dylan Dunne <dylanadunne@gmail.com>
2016-04-29 17:58:54 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,41 +1,37 @@
+$('.fb-button').click(function () {
+    var inputNumber = parseInt($('.fb-input').val());
 
-$(document).ready(function () {
-​
-    $('.fb-button').click(function () {
-        var inputNumber = parseInt($('.fb-input').val());
-​
-        if (isNaN(inputNumber) || inputNumber > 100) {
-​
-            $('.fb-message').text("Yo bro, enter an integer brah, between 1 & 100 yo.");
-​
-        } else {
-​
-            $('.fb-input').empty();
-            $('.fb-message').empty();
-​
-            fizzbuzz(inputNumber);
-​
-            $(".call-out").empty();
-            $(".call-out").append(inputNumber + "?!?");
-        }
-    });
-​
-    function fizzbuzz(inputNumber) {
-​
-        $('ul li').remove();
-​
-        for (var n = 1; n < inputNumber; n++) {
-​
-            n % 5 !== 0 && n % 3 !== 0 ? append(n) : false;
-            n % 15 === 0 ? append('ThizzBuzz') : false;
-            n % 3 === 0 ? append('Thizz') : false;
-            n % 5 === 0 ? append('Buzz') : false;
-​
-        }
+    if (isNaN(inputNumber) || inputNumber > 100) {
+
+        $('.fb-message').text("Yo bro, enter an integer brah, between 1 & 100 yo.");
+
+    } else {
+
+        $('.fb-input').empty();
+        $('.fb-message').empty();
+
+        fizzbuzz(inputNumber);
+
+        $(".call-out").empty();
+        $(".call-out").append(inputNumber + "?!?");
     }
-​
-    function append(item) {
-        $(".fb-list").append('<li>' + item + '</li>');
+});
+
+function fizzbuzz(inputNumber) {
+
+    $('ul li').remove();
+
+    for (var n = 1; n < inputNumber; n++) {
+
+        n % 5 !== 0 && n % 3 !== 0 ? append(n) : false;
+        n % 15 === 0 ? append('ThizzBuzz') : false;
+        n % 3 === 0 ? append('Thizz') : false;
+        n % 5 === 0 ? append('Buzz') : false;
+
     }
-​
+}
+
+function append(item) {
+    $(".fb-list").append('<li>' + item + '</li>');
+}
 });
